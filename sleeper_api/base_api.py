4d6e785b97edc940eb437deaf6d3677c6335c81b
Swapnik Katkoori <katkoor2@msu.edu>
2019-06-10 15:38:18 -0400

    removed exceptions

--- sleeper_api/base_api.py
+++ sleeper_api/base_api.py
@@ -1,20 +1,6 @@
 import requests
 import json
 
-class SleeperBadRequest(Exception):
-	pass
-
-class SleeperNotFoundError(Exception):
-	pass
-
-class SleeperTooManyRequests(Exception):
-	pass
-
-class SleeperInternalServerDown(Exception):
-	pass
-
-class SleeperServiceUnavailable(Exception):
-	pass
 
 class BaseApi():
 	def _call(self, url):
