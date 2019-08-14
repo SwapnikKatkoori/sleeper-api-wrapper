4a63e4e084fb37ff64cd16bcc24bc99734079b99
Swapnik Katkoori <katkoor2@msu.edu>
2019-08-14 18:33:52 -0400

    updated version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="sleeper-api-wrapper",
-    version="1.0.2",
+    version="1.0.3",
     description="A Python API wrapper for Sleeper Fantasy Football, as well as tools to simplify data recieved.",
     long_description=README,
     long_description_content_type="text/markdown",
